b5b358f20d3ab736ba6d33bd6b1e6c4bf94c2998
christiandino <cdino96@gmail.com>
2025-04-09 10:49:42 -0300

    Many-to-Many association with joinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -9,30 +9,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
+
 	// Não iremos usar o List nesse caso específico
-	// O Set representa um conjunto, para garantir que não tera um produto com mais de uma ocorrencia da mesma categoria
-	@Transient
+	// O Set representa um conjunto, para garantir que não tera um produto com mais
+	// de uma ocorrencia da mesma categoria
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>(); // usamos o HashSet e não só Set, pq o set é uma interface, ou seja, ele não pode ser instanciado
-	
+
 	public Product() {
-		
+
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -104,7 +108,5 @@ public class Product implements Serializable{
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
+
 }
